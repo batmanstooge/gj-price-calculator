6c5656bcce2c6aff0a46a9d5ae39e728f73764ed
John Robin Davis <johnrobindavis@Johns-MacBook-Pro.local>
2023-04-01 13:02:07 +0530

    Stored Discounts in firestore

--- src/pages/DiscountsPage.tsx
+++ src/pages/DiscountsPage.tsx
@@ -6,46 +6,59 @@ import {
   IonItem,
   IonLabel,
   IonList,
+  IonLoading,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import { useContext, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
+import { firestore } from "../firebase/firebase";
 import "./Settings.css";
 
 const DiscountsPage: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const { discount, onEditDiscount } = useContext(AppContext);
   const [enteredDiscount, setEnteredDiscount] = useState<any>(discount);
-  const handleDiscountChange = (event: { detail: { value: any } }) => {
+  const handleEditDiscountChange = (event: { detail: { value: any } }) => {
     setEnteredDiscount(event.detail.value);
   };
 
-  const handleDiscountSave = () => {
+  const handleDiscountSave = async () => {
+    setLoading(true);
+    await firestore.collection("discount").doc("discount").set(
+      {
+        discount: +enteredDiscount,
+      },
+      { merge: true }
+    );
+
     if (onEditDiscount) onEditDiscount(enteredDiscount);
+    setLoading(false);
   };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Discounts</IonTitle>
+          <IonTitle>Price</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonList>
           <IonItem>
-            <IonLabel>Edit Discount</IonLabel>
+            <IonLabel>Edit Price</IonLabel>
             <IonInput
               type="number"
               value={enteredDiscount}
-              onIonChange={handleDiscountChange}
+              onIonChange={handleEditDiscountChange}
             />
           </IonItem>
         </IonList>
         <IonButton expand="block" onClick={handleDiscountSave}>
           Save
         </IonButton>
+        <IonLoading isOpen={loading} />
       </IonContent>
     </IonPage>
   );
--- src/pages/PricePage.tsx
+++ src/pages/PricePage.tsx
@@ -6,23 +6,35 @@ import {
   IonItem,
   IonLabel,
   IonList,
+  IonLoading,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import { useContext, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
+import { firestore } from "../firebase/firebase";
 import "./Settings.css";
 
 const PricePage: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const { price, onEditPrice } = useContext(AppContext);
   const [enteredPrice, setEnteredPrice] = useState<any>(price);
   const handleEditPriceChange = (event: { detail: { value: any } }) => {
     setEnteredPrice(event.detail.value);
   };
 
-  const handlePriceSave = () => {
+  const handlePriceSave = async () => {
+    setLoading(true);
+    await firestore.collection("price").doc("price").set(
+      {
+        price: +enteredPrice,
+      },
+      { merge: true }
+    );
+
     if (onEditPrice) onEditPrice(enteredPrice);
+    setLoading(false);
   };
 
   return (
@@ -46,6 +58,7 @@ const PricePage: React.FC = () => {
         <IonButton expand="block" onClick={handlePriceSave}>
           Save
         </IonButton>
+        <IonLoading isOpen={loading} />
       </IonContent>
     </IonPage>
   );
